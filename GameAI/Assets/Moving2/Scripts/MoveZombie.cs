7d349fb5d06a90c010d0da42aac07bf71ba7c51f
MelihCelik00 <melihsafa.c@gmail.com>
2021-02-15 18:02:20 +0300

    integrated slerp rotation

--- GameAI/Assets/Moving2/Scripts/MoveZombie.cs
+++ GameAI/Assets/Moving2/Scripts/MoveZombie.cs
@@ -8,11 +8,14 @@ public class MoveZombie : MonoBehaviour
     public Transform goal;
     public float speed = 0.5f;
     public float accuracy = 1.0f;
-
+    public float rotSpeed = 0.1f;
+    
     private void LateUpdate()
     {
         Vector3 lookAtGoal = new Vector3(goal.position.x, this.transform.position.y, goal.position.z);
-        this.transform.LookAt(lookAtGoal);
+        Vector3 direction = lookAtGoal - this.transform.position;
+        
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation,Quaternion.LookRotation(direction), Time.deltaTime*rotSpeed);
         if (Vector3.Distance(this.transform.position, lookAtGoal) > accuracy)
         {
             this.transform.Translate(0, 0, speed * Time.deltaTime);
